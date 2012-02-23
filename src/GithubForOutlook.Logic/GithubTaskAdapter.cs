707770edd4d09682969b98f78d3c5e809b1ace59
Jake Ginnivan <jake@ginnivan.net>
2012-02-23 19:14:38 +0800

    More structure in place

---
+++ src/GithubForOutlook.Logic/GithubTaskAdapter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using VSTOContrib.Outlook;
+
+namespace GithubForOutlook.Logic
+{
+    public class GithubTaskAdapter
+    {
+        private const string Githubissueid = "GithubIssueId";
+        private readonly TaskItem outlookTask;
+        private bool? isGithubTask;
+
+        public GithubTaskAdapter(TaskItem outlookTask)
+        {
+            this.outlookTask = outlookTask;
+        }
+
+        public bool IsGithubTask
+        {
+            get
+            {
+                if (isGithubTask == null)
+                {
+                    isGithubTask = CheckIfGithubTask();
+                }
+                return isGithubTask.Value;
+            }
+            set
+            {
+                isGithubTask = value;
+                outlookTask.SetPropertyValue(Githubissueid, OlUserPropertyType.olInteger, value, true);
+            }
+        }
+
+        private bool CheckIfGithubTask()
+        {
+            var issueId = outlookTask.GetPropertyValue(Githubissueid, OlUserPropertyType.olInteger, false, o=>(int?)o, null);
+
+            return issueId != null;
+        }
+    }
+}
